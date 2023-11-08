975efee03c48324fe177479c8513e25fa8f7cfbd
hyedo0806 <dohye0806@gmail.com>
2023-11-08 06:24:41 +0000

    holly shit

--- lab3/task_controller.py
+++ lab3/task_controller.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 
 import sys
+import struct
 import pox.openflow.libopenflow_01 as of
+import pox.lib.packet as pkt
 from pox.lib.addresses import IPAddr, IPAddr6, EthAddr
 from scapy.all import * # you can use scapy in this task
+
+from pox.core import core
+from pox.lib.packet.ethernet import ethernet
+from pox.lib.packet.ipv4 import ipv4
+from pox.lib.packet.udp import udp
+from pox.lib.packet.dns import dns
+import pox.openflow.discovery as discovery
+import pox.openflow.spanning_tree as spanning_tree
 
 
 graph = []
@@ -72,38 +82,20 @@ class Graph():
     return path
 
 def addrule(switchname, connection) -> None:
-  print(switchname)
-  print("addrule : ", port)
-  
-  for (s,d),path in routing_table.items():
-
-    print('h'+str(s+1),': ',hosts['h'+str(s+1)]['IP'])
-    print('h'+str(d+1),':', hosts['h'+str(d+1)]['IP'])
-    for idx,p in enumerate(path[1:-1]):
-      print(path)
-      print(switchname, ',', p)
-      if switchname == p:  
-        print("loop")
-        
-        in_port = port[(path[idx], path[idx+1])][1]
-        out_port = port[(path[idx+1], path[idx+2])][0]
-
-        print(p,':', in_port,',',out_port)
-        arp_msg = of.ofp_flow_mod()
-        arp_msg.match = of.ofp_match()
-        arp_msg.match.dl_type = 0x0806
-        arp_msg.match.in_port = in_port
-        arp_msg.actions.append(of.ofp_action_output(port=out_port))
-        connection.send(arp_msg)
-        
-        ip_msg = of.ofp_flow_mod() 
-        ip_msg.match = of.ofp_match()
-        ip_msg.match.dl_type = 0x0800
-        ip_msg.match.in_port = in_port
-        ip_msg.match.nw_src = IPAddr(hosts['h'+str(s+1)]['IP'])
-        ip_msg.match.nw_dst = IPAddr(hosts['h'+str(d+1)]['IP']) 
-        ip_msg.actions.append(of.ofp_action_output(port=out_port))
-        connection.send(ip_msg)
+	
+   #msg = of.ofp_flow_mod()
+   #msg.match.dl_type = 0x0800
+   #msg.match.nw_proto = 17 
+   #msg.match.tp_src = 53  # Source port 53 (DNS query)
+   #msg.match.tp_dst = 53  # Destination port 53 (DNS response)
+   #msg.actions.append(of.ofp_action_output(port=of.OFPP_CONTROLLER))
+   #connection.send(msg)
+   
+   msg = of.ofp_flow_mod()
+   #msg.match.dl_type = 0x0806
+   msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+   connection.send(msg)
+   
         
 
 def make_matrix():
@@ -135,6 +127,8 @@ def make_matrix():
     
 def init(net):
 
+ 
+
   global num_hosts, num_switches, routing_table, hosts, switches
   print("net : ",net) 
   hosts = net['hosts']
@@ -160,18 +154,55 @@ def init(net):
   print("routing table : ", routing_table)
 
 def handlePacket(switchname, event, connection):
-	global bestport
-	packet = event.parsed
-	if not packet.parsed:
-		print('Ignoring incomplete packet')
-		return
+  global bestport
+  packet = event.parsed
+  if not packet.parsed:
+    print('Ignoring incomplete packet')
+    return
+  data = event.data
+  print("_" * 100)
 
-	packetfrags = {}
-	p = packet
-	while p is not None:
-		packetfrags[p.__class__.__name__] = p
-		if isinstance(p, bytes):
-			break
-		p = p.next
-	#print("packet : ", packet.__dict__)
-	print(packet.dump()) # print out unhandled packets
+  hexdump(data)
+  pkt = Packet(data)
+  ether = Ether(data)
+  print(ether.show())
+  
+  print(type(pkt), pkt)
+  print(pkt.name)
+  print(pkt.parent)
+  print(type(ether), ether)
+  print(ether.__dict__)
+  print(ether.summary())
+  print("\n출발지 MAC 주소:", ether.src)
+  print("목적지 MAC 주소:", ether.dst)
+  print("이더넷 타입/프레임 유형:", hex(ether.type))
+  
+  if IP in ether:
+    ip = ether[IP]
+    print("\nIP 헤더:")
+    print(ip.summary())
+    print("\n출발지 IPv6 주소:", ip.src)
+    print("목적지 IPv6 주소:", ip.dst)
+    if Raw in ether:
+      raw = ether[Raw]
+      print(raw.load)
+      decoded_data = raw.load.decode('ascii')
+      
+      
+
+  if ICMPv6ND_RA in ether:
+    ra = ether[ICMPv6ND_RA]
+    print("\nICMPv6 Router Advertisement 메시지:")
+    print("유형:", ra.type)
+    print("코드:", ra.code)
+    print("체크섬:", hex(ra.chksum))
+ 
+  
+  packetfrags = {} 
+  p = packet
+  while p is not None:
+    packetfrags[p.__class__.__name__] = p
+    if isinstance(p, bytes): break
+    p = p.next
+
+  print("dropt : ",packet.dump()) # print out unhandled packets
